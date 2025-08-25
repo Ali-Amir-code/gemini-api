7a77039f212059f37e23d8d87c0adba0ad0a95a9
Ali Amir <muhammadaliamir24@gmail.com>
2025-08-25 12:08:26 +0500

    Test 2

--- api/gemini.js
+++ api/gemini.js
@@ -1,17 +1,28 @@
-import { GoogleGenAI } from "@google/genai";
+import { createPartFromUri, createUserContent, GoogleGenAI } from "@google/genai";
 export default async function handler(req, res) {
+
+    const { query } = req.body;
 
     const ai = new GoogleGenAI({});
 
-    async function main() {
-        const response = await ai.models.generateContent({
-            model: "gemini-2.5-flash",
-            contents: "How does AI work?",
-        });
-        return response.text;
-    }
+    const dataResponse = await fetch('./data.json');
+    const data = await dataResponse.json();
 
-    const response = await main();
 
-    res.status(200).json({ message: response });    
+    const response = await ai.models.generateContent({
+        model: "gemini-2.5-flash",
+        contents: `
+        Query: ${query}
+        Return: A single concise, natural, accurate answer (1–3 short paragraphs). If the draft lacks necessary info, return exactly: Sorry! I am unable to understand your question.\n Please contact Ali Amir directly at muhammadaliamir24@gmail.com.
+        `,
+        config: {
+            thinkingConfig: {
+                thinkingBudget: 0, // Disables thinking
+            },
+            temperature: 0.2,
+            systemInstruction: `You are a personal ai assistant of Ali Amir and is hosted on his website. Your primary task is to assist website visitors by answering their queries on Ali Amir\'s behalf. Here is all the data about Ali Amir. DATA: ${data}\nUse ONLY given information. Do NOT invent facts. Output ONLY the final answer text — no JSON, no commentary, nothing else.`
+        }
+    });
+
+    res.status(200).json({ message: response.text });
 }
